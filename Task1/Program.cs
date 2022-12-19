07f9372a1912c56ae2ce9613cb0e2c75f1a5a377
Iryna <batsianiryna@gmail.com>
2022-12-20 00:23:03 +0300

    add task1 сумма чисел в промежутке от m до n

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int rec(int m, int n)
+{
+    if (m == n)
+        return m;
+    return rec(m, n - 1) + n;
+}
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
